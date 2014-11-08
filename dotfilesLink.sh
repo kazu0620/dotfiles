2512e10805b2cbffa4c9c84103a9c9c70063c3e2
Kazuhiro Sakamoto <kazu620@gmail.com>
2014-11-08 16:17:03 +0900

    .screenrcを追加

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -5,4 +5,5 @@ ln -s ~/dotfiles/.bashrc ~/.bashrc
 ln -s ~/dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/dotfiles/.vim ~/.vim
 ln -s ~/dotfiles/.zsh ~/.zsh
+ln -s ~/dotfiles/.screenrc ~/.screenrc
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
