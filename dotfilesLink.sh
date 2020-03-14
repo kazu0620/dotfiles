0b84a91837bf49af92ffb1b012db2b15094ae702
Kazuhiro Sakamoto <kazu620@gmail.com>
2020-03-14 14:05:29 +0900

    GNU screen -> tmux

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -5,7 +5,7 @@ ln -s ~/dotfiles/.bashrc ~/.bashrc
 ln -s ~/dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/dotfiles/.vim ~/.vim
 ln -s ~/dotfiles/.zsh ~/.zsh
-ln -s ~/dotfiles/.screenrc ~/.screenrc
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.peco ~/.peco
 ln -s ~/dotfiles/.tigrc ~/.tigrc
